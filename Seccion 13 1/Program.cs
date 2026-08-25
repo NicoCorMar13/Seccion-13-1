9b3d55ef793d8a1c7ac93ec35046088709b6ebb0
NicoCorMar13 <nico_betico_13@yahoo.es>
2026-08-25 13:20:04 +0200

    Agregar método multiplicacion

--- Seccion 13 1/Program.cs
+++ Seccion 13 1/Program.cs
@@ -20,5 +20,12 @@
 
             return resultado;
         }
+
+        static double Multiplicacion(double num1, double num2)
+        {
+            double resultado = num1 * num2;
+
+            return resultado;
+        }
     }
 }
